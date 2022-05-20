ef3b65ae5b33fdaae407c6bfd763c75c61653322
Luis Herrera <luis378962@gmail.com>
2022-05-20 15:25:55 -0500

    Se agrego el index de los proyectos

---
+++ salarios.js
@@ -0,0 +1,61 @@
+const colombia = [];
+colombia.push({
+  name: "Juanita",
+  salary: 1000,
+});
+colombia.push({
+  name: "Luis",
+  salary: 500,
+});
+colombia.push({
+  name: "Mateo",
+  salary: 1700,
+});
+colombia.push({
+  name: "Maria",
+  salary: 1400,
+});
+colombia.push({
+  name: "Amanda",
+  salary: 1800,
+});
+colombia.push({
+  name: "Pablo",
+  salary: 700,
+});
+colombia.push({
+  name: "Alfonso",
+  salary: 2000,
+});
+colombia.push({
+  name: "Diana",
+  salary: 1100,
+});
+colombia.push({
+  name: "Frida",
+  salary: 1350,
+});
+colombia.push({
+  name: "Juan",
+  salary: 900,
+});
+colombia.push({
+  name: "Melissa",
+  salary: 1000,
+});
+colombia.push({
+  name: "Carmen",
+  salary: 2500,
+});
+colombia.push({
+  name: "Susana",
+  salary: 1400,
+});
+colombia.push({
+  name: "Laura",
+  salary: 2200,
+});
+colombia.push({
+  name: "Daniela",
+  salary: 2000,
+});
